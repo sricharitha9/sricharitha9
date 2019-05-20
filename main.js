9894edc3259cbfd379c5429fb61341a0ab716389
sri charitha <sricharithadasaraju@gmail.com>
2019-05-20 16:54:55 +0530

    1

---
+++ main.js
@@ -0,0 +1,162 @@
+const main=document.querySelector("main");
+const modalOpen=document.querySelector("#add-employee");
+const modal=document.querySelector("#simplemodal");
+const closeBtn=document.querySelector(".close-btn");
+const submit=document.querySelector(".form-button");
+const resetButton=document.querySelector("#reset");
+var path ="";
+
+const array=[];
+// form data
+
+resetButton.addEventListener("click",()=>{
+    const container=document.querySelectorAll(".container");
+    for (let i of container){
+        i.remove();
+    }
+})
+
+const name=document.querySelector("#name");
+const designation=document.querySelector("#designation");
+const department=document.querySelector("#department");
+const dob=document.querySelector("#dob");
+const number=document.querySelector("#number");
+const email=document.querySelector("#email");
+const address=document.querySelector("#address");
+const image=document.querySelector("#image");
+
+modalOpen.addEventListener("click",()=>{
+    modal.style.display="block"
+})
+
+closeBtn.addEventListener("click",()=>{
+    modal.style.display="none";
+})
+
+window.addEventListener("click",(e)=>{
+    if (e.target==modal){
+        modal.style.display="none"
+    }
+})
+
+
+submit.addEventListener("click",()=>{
+    let div=createEmployee(
+        name.value,
+        designation.value,
+        department.value,
+        dob.value,
+        number.value,
+        email.value,
+        address.value,
+        image.value
+    );
+    const Image=div.image;
+    div=div.employee;
+    const firstchild=div.firstChild;
+    div.classList.add("container");
+    main.appendChild(div);
+    array=localStorage.getItem("array");
+    array.push(div);
+    localStorage.setItem(array);
+    const next = firstchild.nextElementSibling;
+    
+    const next1=next.nextElementSibling;
+    next1.appendChild(Image);
+    next1.style.display="none"
+    next.addEventListener("click",function(){
+        if (next1.style.display==="none"){
+           return next1.style.display="inline-block"
+        }
+    	next1.style.display="none";
+    })
+    modal.style.display="none"
+})
+
+var loadFile = function(event) {
+	
+     path = URL.createObjectURL(event.target.files[0]);
+     
+      
+};
+
+
+function createEmployee(name,designation,department,dob,number,email,address,image) {
+    const employeeRecord=document.createElement("div");
+    employeeRecord.innerHTML=`
+   
+       <button class="butn toggle-button" onclick ="toggle('container')">employee</button>
+    
+     <div>
+    
+    <div>
+        <span class="left">Name:</span>
+        <span class="right">${name}</span>
+    </div>
+    <div>
+        <span class="left">Designation:</span>
+        <span class="right">${designation}</span>
+    </div>
+    <div>
+        <span class="left">Department:</span>
+        <span class="right">${department}</span>
+    </div>
+    <div>
+        <span class="left">Date of Birth:</span>
+        <span class="right">${dob}</span>
+    </div>
+    <div>
+        <span class="left">Contact Number:</span>
+        <span class="right">${number}</span>
+    </div>
+    <div>
+        <span class="left">Email:</span>
+        <span class="right">${email}</span>
+    </div>
+    <div>
+        <span class="left">Address:</span>
+        <span class="right">${address}</span>
+    </div>
+   
+  <button class="button"><i class="fas fa-pencil-alt"></i></button>
+  <button class="button"><i class="fas fa-trash-alt"></i></button> 
+`;
+var image = document.createElement("IMG");
+image.src = path;
+image.id ="gap";
+
+//    employeeRecord.appendChild(image);
+   return {employee:employeeRecord,image:image};
+ 
+    
+}
+function myfun(){
+    alert("working");
+    var a=document.getElementById("number").value;
+    if(a===""){
+    document.getElementById("messages").innerHTML="**please fill mobile number";
+    
+    }
+    else if(isNaN(a)){
+    document.getElementById("messages").innerHTML="**enter only numeric values";
+
+    }
+    else if(a.length<10){
+    document.getElementById("messages").innerHTML="**mobile number must be 10 digits";
+    
+    }
+    else if(a.length>10){
+    document.getElementById("messages").innerHTML="**mobile number must be 10 digits";
+    
+    }
+    else((a.charAt(0)!=9) && (a.charAt(0)!=8) && (a.charAt(0)!=7))
+    {
+    document.getElementById("messages").innerHTML="**mobile number must start with 9 or 6 or 7";
+    
+    }
+    }
+    
+    
+ 
+
+
